0f98edbb71bd11dcc947aad44ba2d33c985343a6
Juleslg <julesradiolg@gmail.com>
2024-08-05 15:12:52 +0100

    test

--- assets/theme.js
+++ assets/theme.js
@@ -2162,10 +2162,10 @@ var Drawer = class extends Modal {
     return this.getAttribute("template") || "drawer-default-template";
   }
   get openFrom() {
-    return this.getAttribute("open-from") || "left";
+    return this.getAttribute("open-from") || "right";
   }
   createEnterAnimationControls() {
-    this.getShadowPartByName("content").style.marginInlineEnd = this.openFrom === "left" ? "auto" : 0;
+    this.getShadowPartByName("content").style.marginInlineStart = this.openFrom === "right" ? "auto" : 0;
     return timeline3([
       [this.getShadowPartByName("overlay"), { opacity: [0, 1] }, { duration: 0.3, easing: [0.645, 0.045, 0.355, 1] }],
       [this.getShadowPartByName("content"), { transform: [`translateX(calc(var(--transform-logical-flip) * ${this.openFrom === "right" ? "100%" : "-100%"}))`, "translateX(0)"] }, { duration: 0.3, at: "<", easing: [0.645, 0.045, 0.355, 1] }]
